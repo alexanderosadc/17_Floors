2ed1c13e114f637d637a81f35c3b5a358a10f37e
Alexander Topalo <saneock19@gmail.com>
2017-11-10 23:48:10 +0200

    Door controller
    Door animations and controller

---
+++ Assets/Scripts/Controllers/CameraMouseMove.js
@@ -0,0 +1,63 @@
+﻿var scrollArea = 5;
+var scrollSpeed = 100;
+var acceleration = 5;
+
+var scrollX = true;
+var scrollY = false;
+
+@HideInInspector
+var maxScrollSpeed = scrollSpeed;
+@HideInInspector
+var moving = false;
+
+function Start() {
+    if (acceleration > 0) {
+        maxScrollSpeed = scrollSpeed;
+        scrollSpeed = 0;
+    }
+}
+
+function Update() {
+    var mPosX = Input.mousePosition.x;
+    var mPosY = Input.mousePosition.y;
+    var normalizer = 0.05;
+
+    moving = false;
+
+    // Do camera movement by mouse position
+    if (scrollX) {
+        if (mPosX < scrollArea) {
+            transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime * normalizer);
+            moving = true;
+        }
+
+        if (mPosX >= Screen.width-scrollArea) {
+            transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime * normalizer);
+            moving = true;
+        }
+    }
+    
+    if (scrollY) {
+        if (mPosY < scrollArea) {
+            transform.Translate(Vector3.up * -scrollSpeed * Time.deltaTime * normalizer);
+            moving = true;
+        }
+
+        if (mPosY >= Screen.height-scrollArea) {
+            transform.Translate(Vector3.up * scrollSpeed * Time.deltaTime * normalizer);
+            moving = true;
+        }
+    }
+
+    if (moving) {
+        if (acceleration > 0 && scrollSpeed <= maxScrollSpeed) {
+            scrollSpeed += acceleration * Time.deltaTime * (normalizer * maxScrollSpeed);
+
+            if(scrollSpeed > maxScrollSpeed)
+                scrollSpeed = maxScrollSpeed;
+        }
+    } else if (acceleration > 0) {
+        scrollSpeed = 0;
+    }
+
+}
---
+++ Assets/Scripts/Controllers/DoorController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public bool blocked = false;
+
+    Animator animator;
+
+    void Start ()
+    {
+        animator = GetComponent<Animator>();
+    }
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            toggle();
+        }
+    }
+
+    void toggle ()
+    {
+        if (isOpened())
+            close();
+        else
+            open();
+    }
+
+    void close()
+    {
+        if (isBlocked())
+            Debug.Log("Door is blocked");
+        else
+            animator.SetBool("opened", false);
+    }
+
+    void open()
+    {
+        if (isBlocked())
+            Debug.Log("Door is blocked");
+        else
+            animator.SetBool("opened", true);
+    }
+
+    bool isOpened()
+    {
+        return animator.GetBool("opened");
+    }
+
+    bool isBlocked()
+    {
+        return blocked;
+    }
+}
